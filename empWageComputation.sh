a325f1543a81d6a94e8f43d032291f304a69fe5a
SatyamIndhane <satyam.indhane@gmail.com>
2021-01-12 14:22:32 +0530

    Employee wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,14 +2,30 @@
 
 #Welcome to the employee wage computation problem statement
 
-var=$((RANDOM%2))
 
-if (( var  == 1 ))
-then
-	echo Employee is Present
-else
-	echo Employee is absent
-fi
+function attendance(){
 
+	var=$((RANDOM%2))
 
+	if (( var  == 1 ))
+	then
+		echo p
+	else
+		echo a
+	fi
+}
 
+function dailywage(){
+	local result="$(attendance)"
+	if [ $result == 'p' ]
+	then
+		wage=$((20*8))
+	elif [ $result == 'a' ]
+	then
+		wage=0
+	fi
+	echo $wage
+}
+
+result="$(dailywage)"
+echo $result
